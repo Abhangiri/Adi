c815bc71652b9986daac3e3dadf52a2a2c204ff0
Abhangiri <abhangiri26@gmail.com>
2023-02-05 19:35:27 +0530

    this is the first assignment

---
+++ python_basic_assignment/date_time.py
@@ -0,0 +1,10 @@
+#Write a Python program to display the current date and time.
+
+
+
+---------------
+
+import datetime
+now = datetime.datetime.now()
+print("current date and time")
+print(now.strftime("%Y-%M-%D %H:%M::%S"))
